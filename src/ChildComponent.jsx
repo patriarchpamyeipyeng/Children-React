9681a1aeacc6ce3b88f2ca2c9d8c204767ccfdbc
patriarchpam <patriarchpam700@gmail.com>
2024-01-29 14:48:28 +0100

    hello why

---
+++ src/ChildComponent.jsx
@@ -0,0 +1,11 @@
+const AdminDashboard = (props) => {
+  
+    return(
+        <>
+        <h1>Guest</h1>
+        <p>{props.children}</p>
+            </>
+    )
+
+}
+export default AdminDashboard
